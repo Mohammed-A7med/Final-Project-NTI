2f4f10b3f0f9bed93050f49a5fc82a9c7e277d82
Mohammed-A7med <mohamedahmedkhalaf68@gmail.com>
2026-02-22 08:05:53 +0200

    feat: enhance Login component with form validation and password toggle
    - integrated react-hook-form for form handling
    - added zod schema validation for email and password
    - implemented show/hide password toggle with Eye/EyeOff icons
    - display validation errors using FieldDescription
    - replaced button with reusable Button component
    - disabled submit button while submitting

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,18 +1,44 @@
-import { Field, FieldLabel } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff } from "lucide-react";
+
+import loginSchema from "./authSchema";
 
 export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: { email: "", password: "" },
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = (data) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <section className="w-full max-w-md mx-auto flex flex-col gap-6">
+      {/* -------------------- Header Section  -------------------- */}
       <div className="flex flex-col gap-2 text-center">
         <h4 className="text-2xl font-bold text-foreground">Welcome Back</h4>
         <p className="text-sm text-muted-foreground">
           Sign in to continue to your account
         </p>
         <p className="text-sm text-muted-foreground">
-          Don’t have an account?{" "}
+          Don’t have an account?
           <Link
             to="/auth/register"
             className="text-primary font-medium hover:text-secondary transition"
@@ -22,32 +48,56 @@ export default function Login() {
         </p>
       </div>
 
-      {/* Form */}
-      <form className="flex flex-col gap-6">
+      {/* -------------------- Login Form --------------------  */}
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
+        {/* ---------- Email Input Field ---------- */}
         <Field>
           <FieldLabel htmlFor="email" className="text-foreground">
             Email
           </FieldLabel>
           <Input
             id="email"
-            type="email"
+            type="text"
             placeholder="Enter your email"
             className="bg-background border-border focus:ring-2 focus:ring-primary"
+            {...register("email")}
           />
+          {errors.email && (
+            <FieldDescription className="text-red-400">
+              {errors.email.message}
+            </FieldDescription>
+          )}
         </Field>
 
+        {/* ----------  Password Input Field  ---------- */}
         <Field>
           <FieldLabel htmlFor="password" className="text-foreground">
             Password
           </FieldLabel>
-          <Input
-            id="password"
-            type="password"
-            placeholder="Enter your password"
-            className="bg-background border-border focus:ring-2 focus:ring-primary"
-          />
+          <div className="relative">
+            <Input
+              id="password"
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter your password"
+              className="bg-background border-border focus:ring-2 focus:ring-primary pr-12"
+              {...register("password")}
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword((prev) => !prev)}
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-sm text-muted-foreground hover:text-primary transition"
+            >
+              {showPassword ? <Eye /> : <EyeOff />}
+            </button>
+          </div>
+          {errors.password && (
+            <FieldDescription className="text-red-400">
+              {errors.password.message}
+            </FieldDescription>
+          )}
         </Field>
 
+        {/* ----------Forgot Password Link ---------- */}
         <Link
           to="/auth/forgot-password"
           className="text-xs text-left text-muted-foreground hover:text-primary hover:underline cursor-pointer transition"
@@ -55,8 +105,10 @@ export default function Login() {
           Forgot your password?
         </Link>
 
+        {/*  ---------- Submit Button  ---------- */}
         <Button
           type="submit"
+          disabled={isSubmitting}
           className="bg-primary text-primary-foreground py-2.5 rounded-lg hover:bg-secondary transition-all duration-300 shadow-sm hover:shadow-md"
         >
           Sign In
