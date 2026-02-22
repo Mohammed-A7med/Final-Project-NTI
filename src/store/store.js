8b8171a98c807fc557dd3a7270c9f43cb5ddb1ae
AmeraaGamal <ameragamal425@gmail.com>
2026-02-22 12:20:00 +0200

    feat : add fitness page ui and some related component

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from './slices/authSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import themeReducer from './slices/themeSlice';
 
 export const store = configureStore({
   reducer: {
-    auth: authReducer,
+    theme: themeReducer,
   },
-})
+});
